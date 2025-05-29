11b292095a187bb7c02cacd30dcad454ef764d3a
Suryansh\ Singh <supremesofficialyt@gmail.com>
2025-05-29 21:00:26 +0530

    Initial local commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import "./index.css"
+function App() {
+  return (
+    <>
+    <div>
+      hi
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue,svelte}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+// tailwind.config.js
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,vue,svelte}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
